52371967d68125851fcca5f707bb0cc3ea7df29e
Zhe Yu <zyu9@ncsu.edu>
2015-09-02 19:30:11 -0400

    code 4

--- hw/code/4/SAonSchaffer.py
+++ hw/code/4/SAonSchaffer.py
@@ -1,7 +1,33 @@
 from __future__ import print_function, unicode_literals
 from __future__ import absolute_import, division
-from random import uniform
+from random import uniform,randint
 import numpy as np
+
+dig=5
+
+class digit(object):
+
+
+    def __init__(self):
+        self.x=[]
+        self.num=0
+        for i in range(0,dig+1):
+            self.x.append(randint(0,9))
+        self.getnum()
+
+    def getnum(self):
+        self.num=0
+        for i in range(1,dig+1):
+            self.num+=self.x[i]*10**(4-i)
+        if self.x[0]%2==1:
+            self.num=-self.num
+
+    def copy(self,other):
+        self.x=other.x
+        self.num=other.num
+
+
+
 
 
 def schaffer(x):
@@ -14,47 +40,45 @@ def energy(x):
     return x/rang
 
 def probability(en,e,T):
-    return np.e**(-((en-e)/abs(en))/T)
+    return np.e**(-((en-e))/T**4)
 
 def neighbor(x):
-    xmax=10**5
-    xmin=-10**5
-    xn=x*(10**uniform(-1,1))
-    if uniform(0,1)>0.5:
-        xn=-xn
-    if xn>xmax:
-        xn=xmax
-    elif xn<xmin:
-        xn=xmin
+    xn=digit()
+    xn.copy(x)
+    i=randint(0,dig)
+    xn.x[i]+=(-1)**(randint(0,1))
+    xn.x[i]=xn.x[i]%10
+    xn.getnum()
     return xn
 
 
 def simulated_annealing(kmax,linewidth):
 
-    s=uniform(-10**5,10**5)
-    sb=s                                # Initialization
-    e=energy(schaffer(s))
+    s=digit()
+    sb=digit()
+    sb.copy(s)                                # Initialization
+    e=energy(schaffer(s.num))
     eb=e
-
+    sn=digit()
 
     for k in range(1,kmax+1):
         T=(kmax+1-k)/kmax
         sn=neighbor(s)
-        en=energy(schaffer(sn))
+        en=energy(schaffer(sn.num))
         if k%linewidth==1:
-            print(", %4d, : %1.10f, " %(k,eb),end="")
+            print(", %4d, : %7.1f, " %(k,sb.num),end="")
         if en<eb:
-            sb=sn
+            sb.copy(sn)
             eb=en
-            s=sn
+            s.copy(sn)
             e=en
             print("!",end="")
         elif en<e:
-            s=sn
+            s.copy(sn)
             e=en
             print("+",end="")
         elif probability(en,e,T)>uniform(0,1):
-            s=sn
+            s.copy(sn)
             e=en
             print("?",end="")
         else:
@@ -66,8 +90,8 @@ def simulated_annealing(kmax,linewidth):
 
 
 if __name__ == '__main__':
-    kmax=1000
+    kmax=100000
     linewidth=25
     sb=simulated_annealing(kmax,linewidth)
-    print("x: %s, " %sb,"f1: %s, " %sb**2,"f2: %s" %(sb-2)**2)
+    print("x: %s, " %sb.num,"f1: %s, " %sb.num**2,"f2: %s" %(sb.num-2)**2,"steps: %s" %kmax)
 
