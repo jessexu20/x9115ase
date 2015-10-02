53258a512a6a4e9091a75cbe7dbb5caa38e6fe6a
Zhe Yu <zyu9@ncsu.edu>
2015-10-02 14:41:14 -0400

    code 7

--- hw/code/7/genetic.py
+++ hw/code/7/genetic.py
@@ -244,9 +244,57 @@ def maxwalksat(model):
     print("Best solution: %s, " %sbest.dec,"f1 and f2: %s, " %sbest.getobj(),
           "step * eval: %s * %s" %(step,eval),", at which eval the best x is found: %s" %evalx)
 
+def differential_evolution(model):
+
+    def mutate(candidates,f,cr,xbest):
+        for i in range(len(candidates)):
+            tmp=range(len(candidates))
+            tmp.remove(i)
+            while True:
+                abc=np.random.choice(tmp,3)
+                a3=[candidates[tt] for tt in abc]
+                xold=candidates[i]
+                r=randint(0,xold.decnum-1)
+                xnew=model()
+                for j in range(xold.decnum):
+                    if random()<cr or j==r:
+                        xnew.dec[j]=a3[0].dec[j]+f*(a3[1].dec[j]-a3[2].dec[j])
+                    else:
+                        xnew.dec[j]=xold.dec[j]
+                if xnew.check(): break
+            if xnew.eval()<xbest.eval():
+                xbest.copy(xnew)
+                print("!",end="")
+            elif xnew.eval()<xold.eval():
+                print("+",end="")
+            else:
+                xnew=xold
+                print(".",end="")
+            yield xnew
+
+
+    nb=c
+    maxtries=t
+    f=0.75
+    cr=0.3
+    xbest=model()
+    candidates=[xbest]
+    for i in range(1,nb):
+        x=model()
+        candidates.append(x)
+        if x.eval()<xbest.eval():
+            xbest.copy(x)
+    for tries in range(maxtries):
+        print(", Retries: %2d, : Best solution: %s, " %(tries,xbest.dec),end="")
+        candidates=[xnew for xnew in mutate(candidates,f,cr,xbest)]
+        print("")
+    print("Best solution: %s, " %xbest.dec,"f1 and f2: %s, " %xbest.getobj(),
+          "evals: %s * %s" %(nb,maxtries))
+
+
 
 if __name__ == '__main__':
-    for model in [Golinski, Schaffer, Osyczka2, Kursawe]:
-        for optimizer in [simulated_annealing, maxwalksat]:
+    for model in [Schaffer, Osyczka2, Kursawe,Golinski]:
+        for optimizer in [simulated_annealing, maxwalksat,differential_evolution]:
             print("Optimizer: %s, " %optimizer.__name__,"Model: %s" %model.__name__)
             optimizer(model)
